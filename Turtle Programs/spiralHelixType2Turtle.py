71afb58a312ddeba892c9a32e8100047b6b9fdb4
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2025-02-13 10:30:20 +0530

    Update Turtle Programs

---
+++ Turtle Programs/spiralHelixType2Turtle.py
@@ -0,0 +1,10 @@
+import turtle
+import math
+t = turtle.Turtle()
+angle=0
+for i in range(250):
+  x=i*math.cos(math.radians(angle))
+  y=i*math.sin(math.radians(angle))
+  t.goto(x,y)
+  angle+=10
+turtle.done()
